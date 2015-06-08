18238f51e591c147df83e990a4bb1078e22af493
Scintillic <Scintillic@users.noreply.github.com>
2015-06-08 16:34:16 -0500

    B40: Added check for edit permission to script generation.

--- DocMAH/Web/RequestProcessor.cs
+++ DocMAH/Web/RequestProcessor.cs
@@ -187,161 +187,164 @@ namespace DocMAH.Web
 		/// </remarks>
 		public void ProcessGenerateInstallScriptRequest(HttpContextBase context)
 		{
-			// There is an open task to make this path configurable.
-			var fileName = Path.Combine(context.Server.MapPath("~"), "ApplicationHelpInstall.xml");
-
-			var settings = new XmlWriterSettings()
+			if (HasEditAuthorization(context))
 			{
-				Indent = true,
-				Encoding = Encoding.UTF8,
-				NewLineChars = Environment.NewLine,
-				IndentChars = "\t"
-			};
+				// There is an open task to make this path configurable.
+				var fileName = Path.Combine(context.Server.MapPath("~"), "ApplicationHelpInstall.xml");
 
-			using (var stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
-			using (var xmlWriter = XmlWriter.Create(stream, settings))
-			{
-				xmlWriter.WriteStartElement(XmlNodeNames.UpdateScriptsElement);
-				xmlWriter.WriteAttributeString(XmlNodeNames.FileSchemaVersionAttribute, _databaseConfiguration.DatabaseSchemaVersion.ToString());
-
-				var nextHelpVersion = _dataStore.Configuration_Read(DatabaseConfiguration.DatabaseHelpVersionKey) + 1;
-				xmlWriter.WriteAttributeString(XmlNodeNames.FileHelpVersionAttribute, nextHelpVersion.ToString());
-				
-				var existingPageIds = new List<int>();
-				var pageUrlScripts = new List<string>();
-				foreach (var page in _dataStore.Page_ReadAll())
+				var settings = new XmlWriterSettings()
 				{
-					// Create insert or update statements for all existing pages.
-					xmlWriter.WriteElementString(
-						XmlNodeNames.UpdateScriptElement,
-						string.Format(Resources.Sql_Page_Install,
-							page.Id,
-							(int)page.PageType,
-							page.ParentPageId.ToNullableSqlValue(),
-							page.Order,
-							page.SourceUrl.ToNullableSqlValue(),
-							page.Title.ToSqlValue(),
-							page.Content.ToSqlValue(),
-							page.VerticalOffset.ToNullableSqlValue(),
-							page.HorizontalOffset.ToNullableSqlValue(),
-							page.OffsetElementId.ToNullableSqlValue(),
-							page.DocImageUrl.ToNullableSqlValue(),
-							page.DocVerticalOffset.ToNullableSqlValue(),
-							page.DocHorizontalOffset.ToNullableSqlValue(),
-							page.IsHidden ? "1" : "0"
-						)
-					);
-					existingPageIds.Add(page.Id);
+					Indent = true,
+					Encoding = Encoding.UTF8,
+					NewLineChars = Environment.NewLine,
+					IndentChars = "\t"
+				};
 
-					// Add existing match URL insert statements to list to be written later.
-					foreach (var matchUrl in page.MatchUrls.Split(' '))
+				using (var stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+				using (var xmlWriter = XmlWriter.Create(stream, settings))
+				{
+					xmlWriter.WriteStartElement(XmlNodeNames.UpdateScriptsElement);
+					xmlWriter.WriteAttributeString(XmlNodeNames.FileSchemaVersionAttribute, _databaseConfiguration.DatabaseSchemaVersion.ToString());
+
+					var nextHelpVersion = _dataStore.Configuration_Read(DatabaseConfiguration.DatabaseHelpVersionKey) + 1;
+					xmlWriter.WriteAttributeString(XmlNodeNames.FileHelpVersionAttribute, nextHelpVersion.ToString());
+
+					var existingPageIds = new List<int>();
+					var pageUrlScripts = new List<string>();
+					foreach (var page in _dataStore.Page_ReadAll())
 					{
-						if (!string.IsNullOrEmpty(matchUrl))
-							pageUrlScripts.Add(
-								string.Format("INSERT DocmahPageUrls VALUES ({0},{1});{2}",
-								matchUrl.ToNullableSqlValue().Replace('*', '%'),
+						// Create insert or update statements for all existing pages.
+						xmlWriter.WriteElementString(
+							XmlNodeNames.UpdateScriptElement,
+							string.Format(Resources.Sql_Page_Install,
 								page.Id,
-								Environment.NewLine
-							));
+								(int)page.PageType,
+								page.ParentPageId.ToNullableSqlValue(),
+								page.Order,
+								page.SourceUrl.ToNullableSqlValue(),
+								page.Title.ToSqlValue(),
+								page.Content.ToSqlValue(),
+								page.VerticalOffset.ToNullableSqlValue(),
+								page.HorizontalOffset.ToNullableSqlValue(),
+								page.OffsetElementId.ToNullableSqlValue(),
+								page.DocImageUrl.ToNullableSqlValue(),
+								page.DocVerticalOffset.ToNullableSqlValue(),
+								page.DocHorizontalOffset.ToNullableSqlValue(),
+								page.IsHidden ? "1" : "0"
+							)
+						);
+						existingPageIds.Add(page.Id);
+
+						// Add existing match URL insert statements to list to be written later.
+						foreach (var matchUrl in page.MatchUrls.Split(' '))
+						{
+							if (!string.IsNullOrEmpty(matchUrl))
+								pageUrlScripts.Add(
+									string.Format("INSERT DocmahPageUrls VALUES ({0},{1});{2}",
+									matchUrl.ToNullableSqlValue().Replace('*', '%'),
+									page.Id,
+									Environment.NewLine
+								));
+						}
 					}
-				}
 
-				// Delete bullets for all deleted pages.
-				if (existingPageIds.Count > 0)
-				{
+					// Delete bullets for all deleted pages.
+					if (existingPageIds.Count > 0)
+					{
+						xmlWriter.WriteElementString(
+							XmlNodeNames.UpdateScriptElement,
+							string.Format("DELETE DocmahBullets WHERE PageId NOT IN({0});{1}", existingPageIds.ToCsv(), Environment.NewLine)
+						);
+					}
+
+					// Delete all page urls as all will be recreated.
 					xmlWriter.WriteElementString(
 						XmlNodeNames.UpdateScriptElement,
-						string.Format("DELETE DocmahBullets WHERE PageId NOT IN({0});{1}", existingPageIds.ToCsv(), Environment.NewLine)
+						string.Format("DELETE DocmahPageUrls;{0}", Environment.NewLine)
 					);
-				}
 
-				// Delete all page urls as all will be recreated.
-				xmlWriter.WriteElementString(
-					XmlNodeNames.UpdateScriptElement,
-					string.Format("DELETE DocmahPageUrls;{0}", Environment.NewLine)
-				);
+					// Delete user settings for all deleted pages. Otherwise, leave them alone.
+					if (existingPageIds.Count > 0)
+					{
+						xmlWriter.WriteElementString(
+							XmlNodeNames.UpdateScriptElement,
+							string.Format("DELETE DocmahUserPageSettings WHERE PageId NOT IN({0});{1}", existingPageIds.ToCsv(), Environment.NewLine)
+						);
+					}
 
-				// Delete user settings for all deleted pages. Otherwise, leave them alone.
-				if (existingPageIds.Count > 0)
-				{
+					if (existingPageIds.Count > 0)
+					{
+						// Delete all deleted pages.
+						xmlWriter.WriteElementString(
+							XmlNodeNames.UpdateScriptElement,
+							string.Format("DELETE DocmahPages WHERE Id NOT IN({0});{1}", existingPageIds.ToCsv(), Environment.NewLine)
+						);
+					}
+
+					// Create insert statements for all page urls.
+					foreach (var script in pageUrlScripts)
+						xmlWriter.WriteElementString(XmlNodeNames.UpdateScriptElement, script);
+
+					// Create insert or update statements for bullets.
+					var existingBulletIds = new List<int>();
+					foreach (var bullet in _dataStore.Bullet_ReadAll())
+					{
+						xmlWriter.WriteElementString(
+							XmlNodeNames.UpdateScriptElement,
+							string.Format(
+								Resources.Sql_Bullet_Install,
+								bullet.Id,
+								bullet.PageId,
+								bullet.Number,
+								bullet.Text.ToSqlValue(),
+								bullet.VerticalOffset,
+								bullet.HorizontalOffset,
+								bullet.OffsetElementId.ToSqlValue(),
+								bullet.DocVerticalOffset.ToNullableSqlValue(),
+								bullet.DocHorizontalOffset.ToNullableSqlValue()
+							)
+						);
+						existingBulletIds.Add(bullet.Id);
+					}
+
+					// Delete any bullets that were deleted for existing pages.
+					if (existingBulletIds.Count > 0)
+					{
+						xmlWriter.WriteElementString(
+							XmlNodeNames.UpdateScriptElement,
+							string.Format("DELETE DocmahBullets WHERE Id NOT IN({0});{1}", existingBulletIds.ToCsv(), Environment.NewLine)
+						);
+					}
+
+					// Update help version
 					xmlWriter.WriteElementString(
 						XmlNodeNames.UpdateScriptElement,
-						string.Format("DELETE DocmahUserPageSettings WHERE PageId NOT IN({0});{1}", existingPageIds.ToCsv(), Environment.NewLine)
+						string.Format("UPDATE DocmahConfiguration SET [Value] = {0} WHERE [Name] = '{1}';{2}", nextHelpVersion, DatabaseConfiguration.DatabaseHelpVersionKey, Environment.NewLine)
 					);
+
+					xmlWriter.Flush();
+					xmlWriter.Close();
+					stream.Close();
 				}
 
-				if (existingPageIds.Count > 0)
+				// Copy file to response stream for good measure.
+				context.Response.Cache.SetNoStore();
+				context.Response.ContentType = "text/plain";
+				context.Response.AddHeader("Content-Disposition", "attachment;filename=ApplicationHelpInstall.xml");
+
+				using (var fileReader = new StreamReader(fileName))
 				{
-					// Delete all deleted pages.
-					xmlWriter.WriteElementString(
-						XmlNodeNames.UpdateScriptElement,
-						string.Format("DELETE DocmahPages WHERE Id NOT IN({0});{1}", existingPageIds.ToCsv(), Environment.NewLine)
-					);
+					while (!fileReader.EndOfStream)
+					{
+						var line = fileReader.ReadLine();
+						context.Response.Write(line);
+						context.Response.Write(Environment.NewLine);
+						context.Response.Flush();
+					}
+					fileReader.Close();
 				}
-
-				// Create insert statements for all page urls.
-				foreach (var script in pageUrlScripts)
-					xmlWriter.WriteElementString(XmlNodeNames.UpdateScriptElement, script);
-
-				// Create insert or update statements for bullets.
-				var existingBulletIds = new List<int>();
-				foreach (var bullet in _dataStore.Bullet_ReadAll())
-				{
-					xmlWriter.WriteElementString(
-						XmlNodeNames.UpdateScriptElement,
-						string.Format(
-							Resources.Sql_Bullet_Install,
-							bullet.Id,
-							bullet.PageId,
-							bullet.Number,
-							bullet.Text.ToSqlValue(),
-							bullet.VerticalOffset,
-							bullet.HorizontalOffset,
-							bullet.OffsetElementId.ToSqlValue(),
-							bullet.DocVerticalOffset.ToNullableSqlValue(),
-							bullet.DocHorizontalOffset.ToNullableSqlValue()
-						)
-					);
-					existingBulletIds.Add(bullet.Id);
-				}
-
-				// Delete any bullets that were deleted for existing pages.
-				if (existingBulletIds.Count > 0)
-				{
-					xmlWriter.WriteElementString(
-						XmlNodeNames.UpdateScriptElement,
-						string.Format("DELETE DocmahBullets WHERE Id NOT IN({0});{1}", existingBulletIds.ToCsv(), Environment.NewLine)
-					);
-				}
-
-				// Update help version
-				xmlWriter.WriteElementString(
-					XmlNodeNames.UpdateScriptElement,
-					string.Format("UPDATE DocmahConfiguration SET [Value] = {0} WHERE [Name] = '{1}';{2}", nextHelpVersion, DatabaseConfiguration.DatabaseHelpVersionKey, Environment.NewLine)
-				);
-
-				xmlWriter.Flush();
-				xmlWriter.Close();
-				stream.Close();
+				context.Response.End();
 			}
-
-			// Copy file to response stream for good measure.
-			context.Response.Cache.SetNoStore();
-			context.Response.ContentType = "text/plain";
-			context.Response.AddHeader("Content-Disposition", "attachment;filename=ApplicationHelpInstall.xml");
-
-			using (var fileReader = new StreamReader(fileName))
-			{
-				while (!fileReader.EndOfStream)
-				{
-					var line = fileReader.ReadLine();
-					context.Response.Write(line);
-					context.Response.Write(Environment.NewLine);
-					context.Response.Flush();
-				}
-				fileReader.Close();
-			}
-			context.Response.End();
 		}
 
 		public void ProcessJavaScriptRequest(HttpContextBase context)
