ae25bf34d815cb59b33fa32b1b6614e40ad48f6f
Scintillic <Scintillic@users.noreply.github.com>
2015-06-26 13:47:10 -0500

    I38: Added test for ReadApplicationSettingsRequestProcessor.

--- DocMAH.UnitTests/Web/Requests/Processors/ReadApplicationSettingsRequestProcessorTestFixture.cs
+++ DocMAH.UnitTests/Web/Requests/Processors/ReadApplicationSettingsRequestProcessorTestFixture.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using DocMAH.Models;
+using DocMAH.Web.Authorization;
+using DocMAH.Web.Requests;
+using DocMAH.Web.Requests.Processors;
 using NUnit.Framework;
 
 namespace DocMAH.UnitTests.Web.Requests.Processors
 {
 	[TestFixture]
-	public class ReadApplicationSettingsRequestProcessorTestFixture
+	public class ReadApplicationSettingsRequestProcessorTestFixture : BaseTestFixture
 	{
 		#region Tests
 
@@ -17,14 +23,24 @@ namespace DocMAH.UnitTests.Web.Requests.Processors
 		public void Process_Success()
 		{
 			// Arrange
-			throw new NotImplementedException();
+			var isAuthorized = true;
+
+			var editAuthorizer = Mocks.Create<IEditAuthorizer>();
+			editAuthorizer.Setup(a => a.Authorize()).Returns(isAuthorized);
+
+			var processor = new ReadApplicationSettingsRequestProcessor(editAuthorizer.Object);
 
 			// Act
-
+			var result = processor.Process(null);
 
 			// Assert
+			Assert.That(result, Is.Not.Null, "A valid ResponseState should be returned.");
+			Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK), "The request should succeed.");
+			Assert.That(result.ContentType, Is.EqualTo(ContentTypes.Json), "JSON should be returned.");
 
-
+			var serializer = new JavaScriptSerializer();
+			var settings = serializer.Deserialize<ApplicationSettings>(result.Content);
+			Assert.That(settings.CanEdit, Is.EqualTo(isAuthorized), "The JSON result should contain the edit authorization result.");
 		}
 
 		#endregion
