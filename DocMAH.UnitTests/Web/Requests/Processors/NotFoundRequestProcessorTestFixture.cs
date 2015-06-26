1f2b9d664788d2b5c286fce34e320621ae6d17d9
Scintillic <Scintillic@users.noreply.github.com>
2015-06-26 13:33:18 -0500

    I38: Added test for NotFoundRequestProcessor.

--- DocMAH.UnitTests/Web/Requests/Processors/NotFoundRequestProcessorTestFixture.cs
+++ DocMAH.UnitTests/Web/Requests/Processors/NotFoundRequestProcessorTestFixture.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using DocMAH.Web.Requests.Processors;
 using NUnit.Framework;
 
 namespace DocMAH.UnitTests.Web.Requests.Processors
@@ -17,14 +19,14 @@ namespace DocMAH.UnitTests.Web.Requests.Processors
 		public void Process_Success()
 		{
 			// Arrange
-			throw new NotImplementedException();
+			var processor = new NotFoundRequestProcessor();
 
 			// Act
-
+			var result = processor.Process(null);
 
 			// Assert
-
-
+			Assert.That(result, Is.Not.Null, "A valid ResponseState should be returned.");
+			Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.NotFound), "The HTTP NotFound status code should be returned.");
 		}
 
 		#endregion
